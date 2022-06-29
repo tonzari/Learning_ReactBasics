91d4b6c54c46e95343fc2808576346ad0de31ab4
Antonio Manzari <antoniomanzari@gmail.com>
2022-06-29 11:43:11 -0400

    added comments for useMemo()

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -9,7 +9,7 @@ function App() {
   // state
   const [allHouses, setAllHouses] = useState([]);
 
-  //load data
+  //load data. This runs one time, when the component is first loaded
   useEffect(() => {
     const fetchHouses = async () => {
       const response = await fetch("/houses.json");
@@ -19,12 +19,28 @@ function App() {
     fetchHouses();
   }, [])
 
+  // Why use useMemo()? I still don't undestand the benefit or necessity of useMemo() here. Study up on useMemo!
+  // Possible Answer: we want to 'remember' the value of featuredHouse. useMemo() will cache this value.
+  // useMemo() will assign either the value returned from the function to the variable,
+  // ...or a previously assigned value.
+  // If our App component is re-rendered due to outside conditions that don't pertain to the featuredHouse,
+  // ...useMemo() will just grab the cached version (so a random new house won't be selected)
+ 
   let featuredHouse = useMemo(() => {
     if (allHouses.length) {
       const randomIndex = Math.floor(Math.random() * allHouses.length);
       return allHouses[randomIndex];
     }
   }, [allHouses])
+
+
+  // same feature, but an approach without react hooks
+  // let featuredHouse = {};
+
+  // if (allHouses.length) {
+  //     const randomIndex = Math.floor(Math.random() * allHouses.length);
+  //     featuredHouse = allHouses[randomIndex];
+  //   }
   
   return (
     <BrowserRouter>
